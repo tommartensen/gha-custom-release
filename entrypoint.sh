070d6ca7f1501871c165abe3b90bd2f93e696a7d
Tom Martensen <tmartens@redhat.com>
2022-09-21 10:38:18 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,17 +17,17 @@ jq . < $EVENT_PATH
 if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*"; then
     VERSION=$(date +%F.%s)
     DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
-    DATA="${DATA} $(printf '"target_commitish":"master",')"
+    DATA="${DATA} $(printf '"target_commitish":"main",')"
     DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
     if [[ "${LOCAL_TEST}" == *"true"* ]]; then
         echo "## [TESTING] Keyword was found but no release was crated."
     else
-        echo $DATA | http POST $URL | jq .
+        echo $DATA | http POST $URL "Authorization: token ${GITHUB_TOKEN}" | jq .
     fi
 else
     echo "Nothing to process."
